d0e349a0293d8f42e913bbc79a1f032534d43e79
YiHangWang <h40403te@yahoo.com.tw>
2017-12-06 12:38:16 +0800

    commit triangle

--- src/test/java/YiHangWangBaseProject/YiHangTest.java
+++ src/test/java/YiHangWangBaseProject/YiHangTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.*;
 public class YiHangTest {
     @Test
     public void getType() throws Exception {
+        YiHang lol = new YiHang(10, 20, 30);
+        System.out.println(lol.getType());
     }
 
 }
