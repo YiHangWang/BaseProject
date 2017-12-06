f0d64f4c5e854fab2865e8b755c85aa1df0e1d9a
YiHangWang <h40403te@yahoo.com.tw>
2017-12-06 12:02:40 +0800

    commit triangle

---
+++ src/test/java/YiHangWangBaseProject/YiHangTest.java
@@ -0,0 +1,12 @@
+package YiHangWangBaseProject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class YiHangTest {
+    @Test
+    public void getType() throws Exception {
+    }
+
+}
